fc45073ab854192f217bcc5cfc7f6d52b891842e
maxogod <maxi18wars@gmail.com>
2025-02-20 18:39:17 -0300

    open go to def on a vertical split

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -34,7 +34,11 @@ lsp.set_preferences({
 lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)  -- go to definition
+    -- go to definition
+    vim.keymap.set("n", "gd", function()
+        vim.cmd("vsplit")
+        vim.lsp.buf.definition()
+    end, opts)
     vim.keymap.set("n", "<C-i>", function() vim.lsp.buf.hover() end, opts)    -- description
     vim.keymap.set("n", "<C-e>", function() vim.diagnostic.open_float() end, opts) -- diagnostics
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts) -- go to next diagnostic
