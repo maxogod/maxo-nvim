14f06c50e469e37b005e2d7b9c71a0fa3ffe8da5
maxogod <maxi18wars@gmail.com>
2025-02-20 19:15:22 -0300

    auto closing and rainbow brackets

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,19 +1,40 @@
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "c",
+        "lua",
+        "rust",
+        "go",
+        "python",
+        "java",
+        "vim",
+        "vimdoc",
+        "query"
+    },
 
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "vim", "vimdoc", "query" },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+    highlight = {
+        enable = true,
 
-  highlight = {
-    enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        additional_vim_regex_highlighting = false,
+    },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    additional_vim_regex_highlighting = false,
-  },
+    indent = {
+        enable = true,
+    },
+
+    rainbow = {
+        enable = true,        -- Enable rainbow brackets
+        extended_mode = true, -- Enable rainbow for more bracket types
+        max_file_lines = 1000,
+    }
 }
