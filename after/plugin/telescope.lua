df150e2188488f5c4fbe5946d613dd06a6829972
maxogod <maxi18wars@gmail.com>
2025-02-23 17:23:02 -0300

    make telescope open file in new tab

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,7 +9,18 @@ require('telescope').setup {
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-p>', function()
+  builtin.find_files({ attach_mappings = function(_, map)
+    map("i", "<CR>", function(prompt_bufnr)
+      local action_state = require("telescope.actions.state")
+      local actions = require("telescope.actions")
+      local entry = action_state.get_selected_entry()
+      actions.close(prompt_bufnr)
+      vim.cmd("tabnew " .. entry[1])
+    end)
+    return true
+  end })
+end, { desc = 'Telescope find files (new tab)' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
