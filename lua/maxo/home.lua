2a469519a143d50aedc09b782b727305b87fe4a8
maxogod <maxi18wars@gmail.com>
2025-03-09 00:29:53 -0300

    Added Home screen auto vertical centering based on term size

--- lua/maxo/home.lua
+++ lua/maxo/home.lua
@@ -17,7 +17,7 @@ vim.cmd("highlight MaxoBlue guifg=#61afef gui=bold")  -- Blue
 vim.cmd("highlight MaxoGreen guifg=#98c379 gui=bold") -- Green
 
 local banner = [[
- ___      ___       __       ___  ___   ______    _____  ___  ___      ___  __     ___      ___ 
+ ___      ___       __       ___  ___   ______    _____  ___  ___      ___  __     ___      ___
 |"  \    /"  |     /""\     |"  \/"  | /    " \  (\"   \|"  \|"  \    /"  ||" \   |"  \    /"  |
  \   \  //   |    /    \     \   \  / // ____  \ |.\\   \    |\   \  //  / ||  |   \   \  //   |
  /\\  \/.    |   /' /\  \     \\  \/ /  /    ) :)|: \.   \\  | \\  \/. ./  |:  |   /\\  \/.    |
@@ -27,6 +27,24 @@ local banner = [[
 ]]
 local message = "press <space>h for help!"
 
+local max_banner_width = 106
+local max_banner_height = 14
+
+local term_width = vim.api.nvim_win_get_width(0)
+local term_height = vim.api.nvim_win_get_height(0)
+
+-- Vertical Positioning
+local gap_height = 2
+local margin_bottom = (term_height / 2) - (3 + gap_height)
+
+local show_banner = term_width >= max_banner_width and term_height >= max_banner_height
+
+if not show_banner then
+    banner = "MaxoNvim"
+    gap_height = 1
+    margin_bottom = (term_height / 2) - gap_height
+end
+
 local centered_banner = center_text(banner)
 local centered_message = center_text(message)
 
@@ -34,11 +52,10 @@ for _, line in ipairs(centered_banner) do
     vim.api.nvim_echo({ { line, "MaxoBlue" } }, false, {})
 end
 
-print("\n\n\n\n")
+print(string.rep("\n", gap_height))
 
 for _, line in ipairs(centered_message) do
     vim.api.nvim_echo({ { line, "MaxoGreen" } }, false, {})
 end
 
-print("\n\n\n")
-
+print(string.rep("\n", margin_bottom))
